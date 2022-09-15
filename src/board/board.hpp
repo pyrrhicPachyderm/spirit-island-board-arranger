27c3fbf39d99b4bbbde86023ec0e6a380d495b5b
Christopher Brown <pyrrhicpachyderm@gmail.com>
2022-09-16 03:35:58 +1200

    Add Board::getNeighbours().

--- src/board/board.hpp
+++ src/board/board.hpp
@@ -1,6 +1,7 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
+#include <stdint.h>
 #include "triangle.hpp"
 
 class Board {
@@ -13,6 +14,53 @@ class Board {
 		
 		Board(Triangle inland, Triangle coast, Triangle ocean):
 			inland(inland), coast(coast), ocean(ocean) {};
+	protected:
+		typedef uint8_t Direction;
+		
+		static Board getNeighbourOverEdge0(const Triangle &tri, Direction edge) {
+			Triangle newCoast = tri.getNeighbours()[edge];
+			Triangle newInland = newCoast.getNeighbours()[(edge+1)%3];
+			Triangle newOcean = newCoast.getNeighbours()[(edge+2)%3];
+			return Board(newInland, newCoast, newOcean);
+		}
+		static Board getNeighbourOverEdge1(const Triangle &tri, Direction edge) {
+			Triangle newInland = tri.getNeighbours()[edge];
+			Triangle newCoast = newInland.getNeighbours()[(edge+2)%3];
+			Triangle newOcean = newCoast.getNeighbours()[edge];
+			return Board(newInland, newCoast, newOcean);
+		}
+		static Board getNeighbourOverEdge2(const Triangle &tri, Direction edge) {
+			Triangle newInland = tri.getNeighbours()[edge];
+			Triangle newCoast = newInland.getNeighbours()[(edge+1)%3];
+			Triangle newOcean = newCoast.getNeighbours()[(edge+2)%3];
+			return Board(newInland, newCoast, newOcean);
+		}
+		static std::array<Board, 3> getNeighboursOverEdge(const Triangle &tri, Direction edge) {
+			//Edge is an index 0-2, the neighbour of tri to generate boards in.
+			return {getNeighbourOverEdge0(tri, edge), getNeighbourOverEdge1(tri, edge), getNeighbourOverEdge2(tri, edge)};
+		}
+		
+		std::array<std::pair<Triangle, Direction>, 3> getOpenEdges() {
+			std::array<Triangle, 3> inlandNeighbours = inland.getNeighbours();
+			Direction inlandToCoastEdge = inlandNeighbours[0] == coast ? 0 : (inlandNeighbours[1] == coast ? 1 : 2);
+			return {
+				std::make_pair(inland, (inlandToCoastEdge + 1) % 3),
+				std::make_pair(inland, (inlandToCoastEdge + 2) % 3),
+				std::make_pair(coast, (inlandToCoastEdge + 2) % 3)
+			};
+		}
+	public:
+		std::array<Board, 9> getNeighbours() {
+			std::array<std::pair<Triangle, Direction>, 3> openEdges = getOpenEdges();
+			std::array<Board, 3> neighbours0 = getNeighboursOverEdge(openEdges[0].first, openEdges[0].second);
+			std::array<Board, 3> neighbours1 = getNeighboursOverEdge(openEdges[1].first, openEdges[1].second);
+			std::array<Board, 3> neighbours2 = getNeighboursOverEdge(openEdges[2].first, openEdges[2].second);
+			return {
+				neighbours0[0], neighbours0[1], neighbours0[2],
+				neighbours1[0], neighbours1[1], neighbours1[2],
+				neighbours2[0], neighbours2[1], neighbours2[2]
+			};
+		}
 };
 
 inline bool operator==(const Board &lhs, const Board &rhs) {
